50362a29aca743344e9abbcf72a43ed68cf37163
amihsan <rumi.apece.du@gmail.com>
2024-09-18 01:21:02 +0200

    workflow terraform security changed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,36 +34,3 @@ output "public_ip" {
   value = aws_instance.app_server.public_ip
 }
 
-
-# provider "aws" {
-#   region = "eu-central-1"
-# }
-
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-00f07845aed8c0ee7" # Amazon Linux 2 AMI
-#   instance_type = "t2.micro"
-#   key_name      = var.key_name
-
-#   tags = {
-#     Name = "FullstackAppServer"
-#   }
-
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo yum install docker -y",
-#       "sudo service docker start",
-#       "sudo usermod -a -G docker ec2-user"
-#     ]
-#   }
-
-#   connection {
-#     type        = "ssh"
-#     user        = "ec2-user"
-#     private_key = file(var.private_key_path)
-#     host        = self.public_ip
-#   }
-# }
-
-# output "public_ip" {
-#   value = aws_instance.app_server.public_ip
-# }
