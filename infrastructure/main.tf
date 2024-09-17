4c4ea44af0749670b3e9f0cfd8b44528daa717d6
amihsan <rumi.apece.du@gmail.com>
2024-09-17 23:39:19 +0200

    ansible playbook updated

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-00f07845aed8c0ee7" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   key_name      = var.key_name
+
+  # Associate the security group
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   tags = {
     Name = "FullstackAppServer"
@@ -30,3 +33,37 @@ resource "aws_instance" "app_server" {
 output "public_ip" {
   value = aws_instance.app_server.public_ip
 }
+
+
+# provider "aws" {
+#   region = "eu-central-1"
+# }
+
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-00f07845aed8c0ee7" # Amazon Linux 2 AMI
+#   instance_type = "t2.micro"
+#   key_name      = var.key_name
+
+#   tags = {
+#     Name = "FullstackAppServer"
+#   }
+
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo yum install docker -y",
+#       "sudo service docker start",
+#       "sudo usermod -a -G docker ec2-user"
+#     ]
+#   }
+
+#   connection {
+#     type        = "ssh"
+#     user        = "ec2-user"
+#     private_key = file(var.private_key_path)
+#     host        = self.public_ip
+#   }
+# }
+
+# output "public_ip" {
+#   value = aws_instance.app_server.public_ip
+# }
---
+++ infrastructure/terraform.tfvars
@@ -0,0 +1,2 @@
+key_name         = "terraform_key"
+private_key_path = "/home/rumi/Downloads/aws_key/terraform_key.pem"
