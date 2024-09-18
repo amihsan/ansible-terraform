1925739a3b0f52c1c0b05b1a9daf79f0996fcf70
amihsan <rumi.apece.du@gmail.com>
2024-09-18 16:49:57 +0200

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -85,27 +85,3 @@ output "public_ip" {
 
 
 
-
-
-
-# provider "aws" {
-#   region = "eu-central-1"
-# }
-
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-00f07845aed8c0ee7" # Amazon Linux 2 AMI
-#   instance_type = "t2.micro"
-#   key_name      = var.key_name
-
-#   # Associate the security group
-#   vpc_security_group_ids = [aws_security_group.app_sg.id]
-
-#   tags = {
-#     Name = "FullstackAppServer"
-#   }
-# }
-
-# output "public_ip" {
-#   value = aws_instance.app_server.public_ip
-# }
-
