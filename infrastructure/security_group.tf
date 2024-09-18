c72f2cd33f5dbd9e0b7ab17ce11bcf5a37a6346a
amihsan <rumi.apece.du@gmail.com>
2024-09-18 10:56:12 +0200

    test

--- infrastructure/security_group.tf
+++ infrastructure/security_group.tf
@@ -16,6 +16,20 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP access from anywhere
   }
 
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow access to React frontend
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow access to Flask backend
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -23,3 +37,30 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
 }
+
+
+# resource "aws_security_group" "app_sg" {
+#   name        = "app-security-group"
+#   description = "Allow inbound traffic for application"
+
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow SSH access from anywhere
+#   }
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP access from anywhere
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+#   }
+# }
