f56fed354bb6d28bcc18c440805c4d4b9e76ccd1
amihsan <rumi.apece.du@gmail.com>
2024-10-04 11:43:46 +0200

    test

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -18,7 +18,9 @@ const Body = () => {
   const [showThreeButtons, setShowThreeButtons] = useState(false);
 
   // Access the API URL from environment variables for docker
-  const baseUrl = window._env_.REACT_APP_API_URL;
+  const baseUrl =
+    process.env.REACT_APP_API_URL || window._env_.REACT_APP_API_URL;
+  // const baseUrl = window._env_.REACT_APP_API_URL;
 
   // Access the API URL from environment variables for local pc
   // const baseUrl = process.env.REACT_APP_API_URL;
