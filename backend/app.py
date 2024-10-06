e1d6f99379857828c99bea157df04584ea20f9d4
amihsan <rumi.apece.du@gmail.com>
2024-10-06 18:33:20 +0200

    test

--- backend/app.py
+++ backend/app.py
@@ -11,15 +11,4 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
 
-# from flask import Flask, jsonify
-# from flask_cors import CORS
 
-# app = Flask(__name__)
-# CORS(app)  # This will allow all domains to access the API
-
-# @app.route('/')
-# def home():
-#     return jsonify(message="Hello from Flask API!")
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
