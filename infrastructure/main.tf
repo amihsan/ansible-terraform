f23991380a5bc552b376039ecc182e9e1a15e15e
amihsan <rumi.apece.du@gmail.com>
2025-03-20 21:00:03 +0100

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -47,7 +47,8 @@ resource "aws_security_group" "travos_sg" {
 resource "aws_instance" "travos_terraform" {
   ami           = "ami-03074cc1b166e8691"  # Example AMI, replace with your own
   instance_type = "t2.micro"  # Free Tier eligible instance
-  security_groups = [aws_security_group.travos_sg.name]
+  key_name               = "travos_terraform_key" 
+  vpc_security_group_ids = [aws_security_group.travos_sg.id]
 
   tags = {
     Name = "TravosInstance"
