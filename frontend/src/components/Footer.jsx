c45dbd3521dc2f91f3438eadef00cf4275dab07d
amihsan <rumi.apece.du@gmail.com>
2025-03-22 16:48:05 +0100

    footer updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,17 +1,3 @@
-// import React from "react";
-// import styles from "./Footer.module.css";
-
-// const Footer = () => {
-//   return (
-//     <footer className={styles.footer}>
-//       <p>&copy; 2023 @ihsan</p>
-//       {"Created for Master Thesis @TU Chemnitz"}
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
 // src/components/Footer.jsx
 import React from "react";
 import styles from "./Footer.module.css";
