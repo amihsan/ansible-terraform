6ca1787d42fdc777661a80d43f85dfbf23b42e4e
amihsan <rumi.apece.du@gmail.com>
2025-03-22 16:45:10 +0100

    footer updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,28 @@
+// import React from "react";
+// import styles from "./Footer.module.css";
+
+// const Footer = () => {
+//   return (
+//     <footer className={styles.footer}>
+//       <p>&copy; 2023 @ihsan</p>
+//       {"Created for Master Thesis @TU Chemnitz"}
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+// src/components/Footer.jsx
 import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Get the current year dynamically
+
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2023 @ihsan</p>
-      {"Created for Master Thesis @TU Chemnitz"}
+      <p>&copy; {currentYear} @Ihsan. All rights reserved. </p>
+      <p>Developed as part of the Master's Thesis @TU Chemnitz.</p>
     </footer>
   );
 };
