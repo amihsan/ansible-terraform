639a21d0ace5933228f60479ed91448d4c0ec554
amihsan <rumi.apece.du@gmail.com>
2025-03-24 17:47:52 +0100

    test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,6 @@ variable "aws_region" { default = "eu-central-1" } # Change this to your preferr
 variable "aws_key_name" { default = "travos_terraform_key" } # Change this to your preferred key pair name
 variable "aws_ami_id" { default = "ami-03074cc1b166e8691" }
 variable "aws_s3_bucket" { default = "travos-terraform-state-bucket" }
-# variable "github_org" { default = "amihsan" } # Change this to your GitHub organization
+# variable "github_org" { default = "amihsan" } # Change this to your GitHub organization or user name
 # variable "github_repo" { default = "*"} # For all repos
 # variable "github_repo" { default = "ansble-terraform"} # Uncomment this line if you want to use a specific repo
